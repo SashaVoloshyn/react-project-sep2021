05375848eae69dd8cfc90f9012c074297319ffee
SashaVoloshyn <sasha.voloshyn359@gmail.com>
2022-02-07 15:18:19 +0200

    структура

--- src/components/FilmCard/FilmCard.js
+++ src/components/FilmCard/FilmCard.js
@@ -4,7 +4,7 @@ const FilmCard = ({film}) => {
     return (
         <div>
             {film.original_title}
-
+            <img src={`https://image.tmdb.org/t/p/w200/${film.poster_path}`} alt={film.title}/>
         </div>
     );
 };
